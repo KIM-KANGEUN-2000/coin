8c9ecefe0196d97b193578a29987c6df68027f20
kkw <kkw@dandysoft.kr>
2024-08-20 23:28:37 +0900

    init

---
+++ app/src/main/java/com/example/coco/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.coco
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import timber.log.Timber
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        // 이 코드를 통해 시스템 UI로 인해 발생하는 여백을 뷰에 적절히 적용할 수 있습니다.
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        Timber.d("onCreate")
+        Timber.tag("MainActivity").d("onCreate")
+        Timber.tag("MainActivity").e("onCreate")
+    }
+
+}
