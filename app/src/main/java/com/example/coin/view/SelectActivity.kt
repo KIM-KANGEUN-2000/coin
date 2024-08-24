7f58d464b8f9f7bc8646f9cf19840115363b12c8
kkw <kkw@dandysoft.kr>
2024-08-24 23:13:22 +0900

    리싸이클러뷰 240824

--- app/src/main/java/com/example/coin/view/SelectActivity.kt
+++ app/src/main/java/com/example/coin/view/SelectActivity.kt
@@ -7,11 +7,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.coin.R
+import com.example.coin.databinding.ActivitySelectBinding
 import com.example.coin.view.adapter.SelectRVAdapter
 import timber.log.Timber
 
 class SelectActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySelectBinding
+
     private val viewModel: SelectViewModel by viewModels()
     // FAQ
 
@@ -20,13 +25,16 @@ class SelectActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_select)
+        binding = ActivitySelectBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         viewModel.getCurrentCoinList()
         viewModel.currentPriceResult.observe(this, Observer {
 
             selectRVAdapter = SelectRVAdapter(this, it)
 
+            binding.coinListRV.adapter = selectRVAdapter
+            binding.coinListRV.layoutManager = LinearLayoutManager(this)
 
             Timber.d(it.toString())
         })
--- app/src/main/java/com/example/coin/view/adapter/SelectRVAdapter.kt
+++ app/src/main/java/com/example/coin/view/adapter/SelectRVAdapter.kt
@@ -1,18 +1,25 @@
 package com.example.coin.view.adapter
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.coin.R
-import com.example.coin.network.model.CurrentPriceList
+import com.example.coin.dataModel.CurrentPriceResult
 
-class SelectRVAdapter(val context: Context, val coinPriceList: List<CurrentPriceList>)
+class SelectRVAdapter(val context: Context, val coinPriceList: List<CurrentPriceResult>)
     :RecyclerView.Adapter<SelectRVAdapter.ViewHolder>() {
 
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val  selectedCoinList = ArrayList<String>()
 
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val coinName : TextView = view.findViewById(R.id.coinName)
+        val coinPriceUpDown : TextView = view.findViewById(R.id.coinPriceUpDown)
+        val likeImage : ImageView = view.findViewById(R.id.likeBtn)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -28,6 +35,41 @@ class SelectRVAdapter(val context: Context, val coinPriceList: List<CurrentPrice
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        holder.coinName.text = coinPriceList[position].coinName
+
+        val fluctate_24H = coinPriceList[position].coinInfo.fluctate_24H
+
+        if(fluctate_24H.contains("-")) {
+            holder.coinPriceUpDown.text = "하락입니다."
+            holder.coinPriceUpDown.setTextColor(Color.parseColor("#114fed"))
+        }else {
+            holder.coinPriceUpDown.text = "상승입니다."
+            holder.coinPriceUpDown.setTextColor(Color.parseColor("#ed2e11"))
+        }
+
+        val likeImage = holder.likeImage
+        val currentCoin = coinPriceList[position].coinName
+
+        // view를 그려줄 때 아래 코드가 없으면 리싸이클러뷰는 재활용을 하기 때문에 likeImage.setOnClickListener에서 선택된 하트가 스크롤시 반복되어 그려진다.
+        if(selectedCoinList.contains(currentCoin)) {
+            selectedCoinList.remove(currentCoin)
+        } else {
+            selectedCoinList.add(currentCoin)
+        }
+
+        likeImage.setOnClickListener {
+
+            if(selectedCoinList.contains(currentCoin)) {
+                // 포함하면
+                selectedCoinList.remove(currentCoin)
+                likeImage.setImageResource(R.drawable.like_grey)
+            } else {
+                // 포함하지 않으면
+                selectedCoinList.add(currentCoin)
+                likeImage.setImageResource(R.drawable.like_red)
+            }
+        }
 
     }
 
