8e35fe7396d03c5a676708188f805ab3899f419a
kkw <kkw@dandysoft.kr>
2024-08-24 21:29:20 +0900

    adapter 240824

--- app/src/main/java/com/example/coin/view/SelectActivity.kt
+++ app/src/main/java/com/example/coin/view/SelectActivity.kt
@@ -6,11 +6,16 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
 import com.example.coin.R
+import com.example.coin.view.adapter.SelectRVAdapter
 import timber.log.Timber
 
 class SelectActivity : AppCompatActivity() {
     private val viewModel: SelectViewModel by viewModels()
+    // FAQ
+
+    private lateinit var selectRVAdapter: SelectRVAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -18,5 +23,12 @@ class SelectActivity : AppCompatActivity() {
         setContentView(R.layout.activity_select)
 
         viewModel.getCurrentCoinList()
+        viewModel.currentPriceResult.observe(this, Observer {
+
+            selectRVAdapter = SelectRVAdapter(this, it)
+
+
+            Timber.d(it.toString())
+        })
     }
 }
--- app/src/main/java/com/example/coin/view/SelectViewModel.kt
+++ app/src/main/java/com/example/coin/view/SelectViewModel.kt
@@ -1,8 +1,13 @@
 package com.example.coin.view
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.coin.dataModel.CurrentPrice
+import com.example.coin.dataModel.CurrentPriceResult
 import com.example.coin.repository.NetWorkRepository
+import com.google.gson.Gson
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
@@ -10,11 +15,37 @@ class SelectViewModel : ViewModel() {
 
     private val netWorkRepository = NetWorkRepository()
 
+    private lateinit var currentPriceResultList: ArrayList<CurrentPriceResult>
+
+    // 데이터변화를 관찰 LiveData
+    private val _currentPriceResult = MutableLiveData<List<CurrentPriceResult>>()
+    val currentPriceResult : LiveData<List<CurrentPriceResult>>
+        get() = _currentPriceResult
+
     fun getCurrentCoinList() = viewModelScope.launch {
 
         val result = netWorkRepository.getCurrentCoinList()
 
         Timber.d(result.toString())
+
+        currentPriceResultList = ArrayList()
+
+        for (coin in result.data) {
+            try {
+                val gson = Gson()
+                val gsonToJson = gson.toJson(result.data.get(coin.key))
+                val gsonFromJson = gson.fromJson(gsonToJson, CurrentPrice::class.java)
+
+                val currentPriceResult = CurrentPriceResult(coin.key, gsonFromJson)
+
+                currentPriceResultList.add(currentPriceResult)
+
+            }catch (e : Exception) {
+                Timber.d(e.toString())
+            }
+        }
+
+        _currentPriceResult.value = currentPriceResultList
 
     }
 
